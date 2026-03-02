504a54e24825bd90a9637159b753293f10fd956a
TheMogoalex <amogollo2@alumno.uned.es>
2026-03-02 17:06:01 +0100

    Add JUnit reporting and artifact archiving for read-only integration tests

--- pipelines/common-steps/integration_readonly.sh
+++ pipelines/common-steps/integration_readonly.sh
@@ -6,5 +6,7 @@ set -x
 # El test usa BASE_URL en env
 export BASE_URL="$BASE_URL"
 
-# Ejecuta SOLO los tests marcados como read-only
-python3 -m pytest -q -m readonly test/integration/todoApiTest.py
+mkdir -p reports
+
+python3 -m pytest -q -m readonly test/integration/todoApiTest.py \
+  --junitxml=reports/integration-readonly-junit.xml
